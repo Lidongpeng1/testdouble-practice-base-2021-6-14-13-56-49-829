bababd0717055dd14b06dd7b2e43604b8838091e
ethan <liet@cargosmart.com>
2021-06-16 19:25:19 +0800

    测试用例1，Check if logNewTrade() of auditService called when execute createTrade() of TradeService

--- src/test/java/com/tw/comprehensive/TradingServiceTest.java
+++ src/test/java/com/tw/comprehensive/TradingServiceTest.java
@@ -1,7 +1,30 @@
 package com.tw.comprehensive;
 
+import org.junit.jupiter.api.Test;
+import org.mockito.*;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class TradingServiceTest {
 
+
+    @Test
+    public void should_call_logNewTrade_when_use_createTrade(){
+        // given
+        AuditService auditService = new AuditService();
+        AuditService spyAuditService = Mockito.spy(auditService);
+
+        TradingService tradingService = new TradingService(new TradeRepository(),spyAuditService);
+
+        // when
+        Trade trade = new Trade("King","Queue");
+        tradingService.createTrade(trade);
+
+        // then
+        verify(spyAuditService).logNewTrade(trade);
+
+    }
 }
